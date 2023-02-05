fcbbf1535e8c4383569622a5e3208172c4b3bb34
Jake Wickstrom <jtwickstrom@gmail.com>
2023-02-05 15:58:36 -0800

    update types

---
+++ src/bin/protobatcher.ts
@@ -0,0 +1,128 @@
+import { NS } from "Bitburner"
+import { TermLogger } from "/lib/Helpers"
+
+const HACK_SCRIPT = '/bin/hack-once.js'
+const GROW_SCRIPT = '/bin/grow-once.js'
+const WEAKEN_SCRIPT = '/bin/weaken-once.js'
+
+/** 
+ * A simple proto-batcher to run on home
+ * @param {NS} ns 
+ */
+export async function main(ns: NS) {
+
+  let logger = new TermLogger(ns)
+
+  if(typeof ns.args[0] !== 'string') {
+    logger.err(`Target server ${ns.args[0]} is not a string! Exiting...`)
+    return
+  }
+
+	// the server we are going to hack
+	let target = ns.args[0]
+
+	let po = ns.getPlayer()
+	let home = ns.getServer()
+	let so = ns.getServer(target)
+
+	// some instinct tells me this is the optimal steal %, 
+	// but I don't have energy to prove it rn
+	let targetSteal = 1 - (1 / Math.E)
+
+	while(!isPrepared(so)) {
+
+		while(!isMinSecurity(so)) {
+			let pid = ns.run(WEAKEN_SCRIPT, getMaxThreads(ns, WEAKEN_SCRIPT, home), target)
+			
+			while(ns.isRunning(pid, home.hostname)) {
+				await ns.sleep(5)
+			}
+
+			await ns.sleep(5)
+		}
+		
+		if(!isMaxMoney(so)) {
+			let pid = ns.run(GROW_SCRIPT, getMaxThreads(ns, GROW_SCRIPT, home), target)
+			
+			while(ns.isRunning(pid, home.hostname)) {
+				await ns.sleep(5)
+			}
+		}
+
+		await ns.sleep(5)
+	}
+
+	ns.tprint(`${so.hostname} has been successfully prepared!`)
+
+	while(true) {
+		
+		let ht = ns.formulas.hacking.hackTime(so, po)
+		let wt = ns.formulas.hacking.weakenTime(so, po)
+		let gt = ns.formulas.hacking.growTime(so, po)
+
+		let hwRatio = Math.ceil(ns.hackAnalyzeSecurity(1, target, home.cpuCores) / ns.weakenAnalyze(1, home.cpuCores))
+		let gwRatio = Math.ceil(ns.growthAnalyzeSecurity(1, target, home.cpuCores) / ns.weakenAnalyze(1, home.cpuCores))
+
+		let hgRatio = ns.hackAnalyze()
+
+		// HWGW - 200
+
+		let hpid1 = ns.run(HACK_SCRIPT, 1, target)
+
+		ns.sleep(gt + 200)
+
+		let wpid1 = ns.run(WEAKEN_SCRIPT, 1 * hwRatio, target)
+
+		ns.sleep()
+
+		let gpid1 = ns.run(GROW_SCRIPT)
+	}
+
+	
+}
+
+async function runAfter(ns, script, threads, delay, ...args) {
+	setTimeout(() => { return ns.run(script, threads, ...args) }, delay)
+}
+
+/**
+ * Returns true if the server is prepared (max money & min security)
+ * @param {Server} so 
+ */
+function isPrepared(so) {
+	return isMinSecurity(so) && isMaxMoney(so)
+}
+
+/**
+ * Returns true if the server is at minimum security
+ * @param {NS} ns 
+ * @param {Server} so 
+ */
+function isMinSecurity(so) {
+	return so.minDifficulty === so.hackDifficulty
+}
+
+/**
+ * Returns true if the server is at maximum money
+ * @param {Server} so 
+ */
+function isMaxMoney(so) {
+	return so.moneyMax === so.moneyAvailable
+}
+
+/**
+ * Returns the amount of RAM available on the server
+ * @param {Server} so 
+ */
+function getAvailableRam(so) {
+	return so.maxRam - so.ramUsed
+}
+
+/**
+ * Returns the amount of RAM available on the server
+ * @param {string} script
+ * @param {Server} so 
+ */
+function getMaxThreads(ns, script, so) {
+	return Math.floor(getAvailableRam(so) / ns.getScriptRam(script))
+}
